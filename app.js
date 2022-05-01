030521fe66c66eda403dbf246b3ff37532a9ec05
vikas namdev <vikasnamdev00786@gmail.com>
2022-05-02 02:33:57 +0530

    done

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-var btnTranslate = document.querySelector("#btn-translate #txt-input")
-var txtinput = document.querySelector("#txt-input")
-var txtoutput = document.querySelector("#output")
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
+var txtOutput = document.querySelector("#output")
+//var url1 = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+ var url1 = "https://api.funtranslations.com/translate/minion.json"
 
-console.log(txtinput)
-console.log(txtoutput)
-
-function clicker (){ 
-    console.log("clicked! ")
-    console.log("input", txtinput.value )
-
+function getUrl(text){
+    return url1 +"?" + "text=" + text
+}
+function erroHandler (error) {
+    console.log(error)
+    alert("something wrong with server so try after some time")
+}
+function clicker(){ 
+    var inputText = txtInput.value
+        fetch(getUrl(inputText))
+        .then (response => response.json())
+        .then (response => {
+            var translateText = response.contents.translated;
+            txtOutput.innerText = translateText
+            
+        })
+        .catch(erroHandler) 
+        
 }
 
-
-addEventListener("click",clicker )
+addEventListener("click", clicker )
